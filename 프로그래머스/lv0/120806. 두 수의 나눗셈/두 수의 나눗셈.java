b8516c77b81ee4714fc54a5f88a57061a67ba97b
김성규 <114158190+KSungGyu@users.noreply.github.com>
2023-01-30 18:56:00 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = (int)((double)num1/num2*1000);
-        // num1에 num2를 나누고 1000을 곱한값을 double로 형변환 후 int로 다시 형변환해서 정수부분만
         return answer;
     }
 }
